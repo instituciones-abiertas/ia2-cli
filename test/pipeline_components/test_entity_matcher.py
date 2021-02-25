94078c19a7dde0390d5fcd5c93eb1e8326907809
Santiago Botta <sgobotta@gmail.com>
2021-02-25 19:02:49 -0300

    EntityMatcher: Se implementan tests

---
+++ test/pipeline_components/test_entity_matcher.py
@@ -0,0 +1,62 @@
+import spacy
+import unittest
+
+from pipeline_components.entity_matcher import (
+    EntityMatcher,
+    matcher_patterns,
+    first_left_nbors, second_left_nbors, first_right_nbors)
+from pipeline_components.entity_ruler import ruler_patterns
+from spacy.pipeline import EntityRuler
+from spacy.tokens import Span
+from test.support.env_case import get_model_path
+
+class EntityMatcherTest(unittest.TestCase):
+    def setUp(self):
+        self.nlp = spacy.load(get_model_path())
+        ruler = EntityRuler(self.nlp, overwrite_ents=True)
+        ruler.add_patterns(ruler_patterns)
+        self.nlp.add_pipe(ruler)
+        entity_matcher = EntityMatcher(self.nlp, matcher_patterns)
+        self.nlp.add_pipe(entity_matcher)
+
+    def test_an_entity_matcher_cleans_out_first_left_nbor_num_entities(self):
+        base_test_sentece = "Tipo de audiencia: Audiencia de admisibilidad de la prueba ({} 210 Código Procesal Penal de la CABA, en adelante CPPCABA)."
+
+        for nbor_word in first_left_nbors:
+            test_sentence = base_test_sentece.format(nbor_word)
+            doc = self.nlp(test_sentence)
+            # Checks that the text is tokenized the way we expect, so that we
+            # can correctly pick up a span with text "210"
+            a_like_num_span = Span(doc, 12, 13, "NUM")
+            self.assertEqual(a_like_num_span.text, "210")
+            # Asserts there is no num span in this doc entities
+            self.assertNotIn(a_like_num_span, doc.ents)
+
+    def test_an_entity_matcher_cleans_out_second_left_nbor_num_entities(self):
+        base_test_sentece = "Tipo de audiencia: Audiencia de admisibilidad de la prueba ({}. 210 Código Procesal Penal de la CABA, en adelante CPPCABA)."
+
+        for nbor_word in second_left_nbors:
+            test_sentence = base_test_sentece.format(nbor_word)
+            doc = self.nlp(test_sentence)
+            # Checks that the text is tokenized the way we expect, so that we
+            # can correctly pick up a span with text "210"
+            a_like_num_span = Span(doc, 13, 14, "NUM")
+            self.assertEqual(a_like_num_span.text, "210")
+            # Asserts there is no num span in this doc entities
+            self.assertNotIn(a_like_num_span, doc.ents)
+
+    def test_an_entity_matcher_cleans_out_first_right_nbor_num_entities(self):
+        base_test_sentece = "Evidencia en Audiencia de admisibilidad para la prueba (Se encuentra a 210 {}, establecido en el Código Procesal Penal de la CABA, en adelante CPPCABA)."
+
+        for nbor_word in first_right_nbors:
+            test_sentence = base_test_sentece.format(nbor_word)
+            doc = self.nlp(test_sentence)
+            # Checks that the text is tokenized the way we expect, so that we
+            # can correctly pick up a span with text "210"
+            a_like_num_span = Span(doc, 12, 13, "NUM")
+            self.assertEqual(a_like_num_span.text, "210")
+            # Asserts there is no num span in this doc entities
+            self.assertNotIn(a_like_num_span, doc.ents)
+
+if __name__ == "__main__":
+    unittest.main()
