b196c46dfc8b43c3f8148f8e899287c14249cd01
José Luis Di Biase <josx@interorganic.com.ar>
2021-02-26 16:26:03 -0300

    Tests: Moving test setup to support lib
    Signed-off-by: José Luis Di Biase <josx@interorganic.com.ar>

--- test/support/env_case.py
+++ test/support/env_case.py
@@ -1,4 +1,11 @@
 import os
+import spacy
+from spacy.pipeline import EntityRuler
+from pipeline_components.entity_ruler import ruler_patterns
+from pipeline_components.entity_matcher import (
+    EntityMatcher,
+    matcher_patterns,
+)
 
 
 def get_model_path():
@@ -10,3 +17,17 @@ def get_model_path():
     if model_path is None:
         raise OSError("TEST_MODEL_FILE environment is not set.")
     return model_path
+
+
+def setup_model():
+    """
+    Retrieve model with current pipeline
+    Returns NLP
+    """
+    nlp = spacy.load(get_model_path())
+    ruler = EntityRuler(nlp, overwrite_ents=True)
+    ruler.add_patterns(ruler_patterns)
+    nlp.add_pipe(ruler)
+    entity_matcher = EntityMatcher(nlp, matcher_patterns)
+    nlp.add_pipe(entity_matcher)
+    return nlp
