d903e2f1a65b4b635eac20cccd0f9cab353a0352
Santiago <santiago@camba.coop>
2021-03-08 11:55:41 -0300

    EntityMatcher: improves NUM matching mechanism by using patterns
    + Complex alphabetical numbers like 'tres mil quinientos noventa y dos' are now detected and labeled as NUM

--- pipeline_components/entity_matcher.py
+++ pipeline_components/entity_matcher.py
@@ -1,9 +1,14 @@
 from spacy.matcher import Matcher
 from spacy.tokens import Span
+from spacy.lang.es.lex_attrs import _num_words
+from spacy.util import filter_spans
 
-matcher_patterns = {
-    "NUM": [{"LIKE_NUM": True}],
-}
+num_words = _num_words + [
+    "quinientos",
+    "quinientas",
+    "ochocientos",
+    "ochocientas",
+]
 
 first_left_nbors = [
     "página",
@@ -49,7 +54,7 @@ def exist_n_token(token_index, nbor_position, document_length):
     document
     """
     index = token_index + nbor_position
-    return index > 0 and index <= document_length
+    return index >= 0 and index < document_length
 
 
 def not_in_nbor(document_length, span, ent_name, word_list, nbor_position):
@@ -64,11 +69,12 @@ def not_in_nbor(document_length, span, ent_name, word_list, nbor_position):
     :param nbor_position: An integer representing a number of characters to the
     left (negative values) or to the right (positive values)
     """
-    return (
-        span.label_ == ent_name
-        and exist_n_token(span[0].i, nbor_position, document_length)
-        and span[0].nbor(nbor_position).text not in word_list
-    )
+    if exist_n_token(span[0].i, nbor_position, document_length):
+        return (
+            span.label_ == ent_name
+            and span[0].nbor(nbor_position).text not in word_list
+        )
+    return True
 
 
 def is_start_of_span_contained(span, target_span):
@@ -90,6 +96,20 @@ def overlaps(span, span_list):
     return False
 
 
+matcher_patterns = [
+    # Multi-num tokens
+    [
+        {"LOWER": {"IN": num_words}, "OP": "+"},
+        {"ORTH": 'y', "OP": "*"},
+        {"LOWER": {"IN": num_words}, "OP": "+"}
+    ],
+    # Single num tokens
+    [{"LOWER": {"IN": num_words}, "OP": "+"}],
+    # Digit-like words
+    [{"IS_DIGIT": True}],
+]
+
+
 class EntityMatcher(object):
     """
     EntityMatcher: matches contexts around "NUM" entities in a Document and
@@ -100,20 +120,21 @@ class EntityMatcher(object):
 
     def __init__(self, nlp, matcher_patterns=matcher_patterns):
         self.nlp = nlp
-        self.matcher = Matcher(self.nlp.vocab)
+        self.matcher = Matcher(self.nlp.vocab, validate=True)
         # Adds patterns to the Matcher pipeline
-        for match_id, pattern in matcher_patterns.items():
-            self.matcher.add(match_id, None, pattern)
+        for pattern in matcher_patterns:
+            self.matcher.add("NUM", [pattern], on_match=None)
 
     def __call__(self, doc):
         matches = self.matcher(doc)
 
-        for match_id, start, end in matches:
-            document_length = len(doc)
-            label = self.nlp.vocab.strings[match_id]
-            span = Span(doc, start, end, label)
+        matched_spans = [Span(doc, start, end, self.nlp.vocab.strings[match_id]) for match_id, start, end in matches]
+        filtered_spans = filter_spans(matched_spans)
 
-            def does_not_have_first_left_nbor():
+        for span in filtered_spans:
+            document_length = len(doc)
+
+            def does_not_have_first_left_nbor(document_length, span):
                 return not_in_nbor(
                     document_length,
                     span,
@@ -122,7 +143,7 @@ class EntityMatcher(object):
                     -1,
                 )
 
-            def does_not_have_second_left_nbor():
+            def does_not_have_second_left_nbor(document_length, span):
                 return not_in_nbor(
                     document_length,
                     span,
@@ -131,13 +152,19 @@ class EntityMatcher(object):
                     -2,
                 )
 
-            def does_not_have_first_right_nbor():
+            def does_not_have_first_right_nbor(document_length, span):
                 return not_in_nbor(document_length, span, "NUM", first_right_nbors, 1)
 
             if (
-                does_not_have_first_left_nbor()
-                and does_not_have_second_left_nbor()
-                and does_not_have_first_right_nbor()
+                does_not_have_first_left_nbor(document_length, span)
+                and does_not_have_second_left_nbor(document_length, span)
+                and does_not_have_first_right_nbor(document_length, span)
             ) and not overlaps(span, doc.ents):
                 doc.ents = list(doc.ents) + [span]
+            else:
+                # FIXME this one ent has been discarded by the nbor word lists.
+                # We should consider assigning them to an entity, or filter them
+                # somewhere else
+                print(f"[FIXME] Should process this span as another ent: `{span}`")
+
         return doc
